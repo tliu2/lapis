161a996701717febf077dd6b30e8f1f30e93ad73
liu <liu@192.168.1.31>
2019-05-03 15:55:48 +0200

    Fix story 3.0 form and query issues.

--- lapis/src/business/PromotionManager.java
+++ lapis/src/business/PromotionManager.java
@@ -15,7 +15,8 @@ public class PromotionManager {
 	public List<Promotion> readPromotionFromUniversityYear(int id) {
 		Session session = DBConnection.getSession();
 		Transaction readTransaction = session.beginTransaction();
-		Query readQuery = session.createQuery("from Promotion p where p.year.id = :" + id);
+		Query readQuery = session.createQuery("from Promotion p where p.year.id = :id");
+		readQuery.setInteger("id", id);
 		List result = readQuery.list();
 		readTransaction.commit();
 
@@ -25,8 +26,12 @@ public class PromotionManager {
 	public int getIdFromUniversityYearString(String year) {
 		int result = 0;
 		Session session = DBConnection.getSession();
+		String[] split = year.split("-");
+		
 		Transaction readTransaction = session.beginTransaction();
-		Query readQuery = session.createQuery("from UniversityYear p where p.year = :" + year);
+		Query readQuery = session.createQuery("from UniversityYear y where y.first = :first and y.last = :last");
+		readQuery.setString("first", split[0]);
+		readQuery.setString("last", split[1]);
 		List resultQuery = readQuery.list();
 		result = ((UniversityYear) resultQuery.get(0)).getId();
 		readTransaction.commit();
