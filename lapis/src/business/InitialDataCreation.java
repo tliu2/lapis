2101f05c47e19baf0eb823b9bb250f00e606cd13
mvois <mvois@DESKTOP-G2EU9TO>
2019-05-06 15:25:26 +0200

    Story 11 part 1

--- lapis/src/business/InitialDataCreation.java
+++ lapis/src/business/InitialDataCreation.java
@@ -621,9 +621,88 @@ public class InitialDataCreation {
 		}
 	}
 
+	public static void generateLanguage() {
+		List<Language> languageList = new ArrayList<Language>();
+		
+		Language java = new Language("Java");
+		languageList.add(java);
+		
+		Language c = new Language("C");
+		languageList.add(c);
+		
+		Language css = new Language("css");
+		languageList.add(css);
+		
+		Language xhtml = new Language("XHTML");
+		languageList.add(xhtml);
+		
+		Language xml = new Language("XML");
+		languageList.add(xml);
+		
+		Language sql = new Language("SQL");
+		languageList.add(sql);
+		
+		Language html = new Language("HTML");
+		languageList.add(html);
+		
+		Language php = new Language("PHP");
+		languageList.add(php);
+		
+		Language matlab = new Language("MATLAB");
+		languageList.add(matlab);
+		
+		Language arduino = new Language("Arduino");
+		languageList.add(arduino);
+		
+		Language openGl = new Language("OpenGL");
+		languageList.add(openGl);
+		
+		Language android = new Language("Android");
+		languageList.add(android);
+		
+		Language vba = new Language("VBA");
+		languageList.add(vba);
+		
+		Language r = new Language("R");
+		languageList.add(r);
+		
+		Language python = new Language("Python");
+		languageList.add(python);
+		
+		Language drRacket = new Language("Dr.Racket");
+		languageList.add(drRacket);
+		
+		Language tex = new Language("Tex");
+		languageList.add(tex);
+		
+		Language lisp = new Language("Lisp");
+		languageList.add(lisp);
+		
+		Language vhdl = new Language("VHDL");
+		languageList.add(vhdl);
+		
+		Language scheme = new Language("SCHEME");
+		languageList.add(scheme);
+		
+		Language cPlusPlus = new Language("C++");
+		languageList.add(cPlusPlus);
+		
+		Language cSharp = new Language("C#");
+		languageList.add(cSharp);
+		
+		Language shell = new Language("SHELL");
+		languageList.add(shell);
+		
+		Language javaScript = new Language("JavaScript");
+		languageList.add(javaScript);
+		
+	}
+	
 	public static void main(String[] args) {
 
 		InitTestData();
+		
+		generateLanguage();
 
 		/*
 		 * 
