ac16e2258912864971b7586e58dfe5ee89a2d09c
oumss <36635145+oumss@users.noreply.github.com>
2019-05-07 12:07:59 +0200

    story 12.1 domains

--- lapis/src/business/DomainDAO.java
+++ lapis/src/business/DomainDAO.java
@@ -1,9 +1,13 @@
 package business;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import persistence.Domain;
+import persistence.UniversityYear;
 
 public class DomainDAO {
 
@@ -13,6 +17,17 @@ public class DomainDAO {
 		Domain domain = new Domain(name);
 		session.persist(domain);
 		readTransaction.commit();
+	}
+	
+	public List<Domain> readAllDomains() {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+
+		Query readQuery = session.createQuery("from Domain");
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
 	}
 
 }
