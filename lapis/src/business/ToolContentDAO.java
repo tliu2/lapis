f0aa73636d9f3cbfc9d01efc6020932a01ca44d0
mvois <mvois@DESKTOP-G2EU9TO>
2019-05-07 13:32:48 +0200

    added readAll for ToolDAO and ToolContentDAO

--- lapis/src/business/ToolContentDAO.java
+++ lapis/src/business/ToolContentDAO.java
@@ -1,8 +1,12 @@
 package business;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import persistence.Course;
 import persistence.Tool;
 import persistence.ToolContent;
 
@@ -14,5 +18,16 @@ public class ToolContentDAO {
 		ToolContent toolcontent = new ToolContent(tool, name);
 		session.persist(toolcontent);
 		readTransaction.commit();
+	}
+	
+	public List<ToolContent> readAllToolContents() {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+
+		Query readQuery = session.createQuery("from ToolContent");
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
 	}
 } 
