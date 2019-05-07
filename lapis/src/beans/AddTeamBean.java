afdbea1729d6731950ce49eae1c7246fcbfe3e01
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-05-07 15:35:57 +0200

    Story 4

---
+++ lapis/src/beans/AddTeamBean.java
@@ -0,0 +1,314 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import business.CourseDAO;
+import business.ProjectDAO;
+import business.PromotionDAO;
+import business.TeamDAO;
+import business.UniversityYearDAO;
+import persistence.Course;
+import persistence.Evaluation;
+import persistence.Project;
+import persistence.Promotion;
+import persistence.UniversityYear;
+
+@ManagedBean
+@ViewScoped
+public class AddTeamBean {
+
+	private Map<String, List<String>> data = new HashMap<String, List<String>>();
+	private Map<String, List<String>> dataPromo = new HashMap<String, List<String>>();
+	private Map<String, List<String>> dataProject = new HashMap<String, List<String>>();
+
+	private String year;
+	private String promo;
+	private String course;
+	private String project;
+
+	private int nbTeams = 1;
+	private int min = 1;
+	private int max = 10;
+	
+	private List<String> years;
+	private List<String> promotions;
+	private List<String> courses;
+	private List<String> projects;
+
+	private List<Evaluation> evaluations = new ArrayList<Evaluation>();
+
+	// private StudentDAO studentDAO = new StudentDAO();
+	private ProjectDAO projectDAO = new ProjectDAO();
+	private CourseDAO courseDAO = new CourseDAO();
+	private PromotionDAO promoDAO = new PromotionDAO();
+	private UniversityYearDAO yearDAO = new UniversityYearDAO();
+	private TeamDAO teamDAO = new TeamDAO();
+	
+	public AddTeamBean() {
+
+	}
+
+	@PostConstruct
+	public void init() {
+
+		List<UniversityYear> allYears = yearDAO.readAllUniversityYears();
+		years = new ArrayList<String>();
+		for (UniversityYear universityYear : allYears) {
+			years.add(universityYear.toString());
+		}
+
+		for (UniversityYear universityYear : allYears) {
+			int id = universityYear.getId();
+			List<Promotion> promos = promoDAO.readPromoByYearId(id);
+			List<String> promoList = new ArrayList<String>();
+			for (Promotion promo : promos) {
+				promoList.add(promo.toString());
+			}
+			data.put(universityYear.toString(), promoList);
+		}
+
+		List<Promotion> allPromos = promoDAO.readAllPromo();
+		for (Promotion promotion : allPromos) {
+			int id = promotion.getId();
+			List<Course> courses = courseDAO.readCourseByPromoId(id);
+			List<String> courseList = new ArrayList<String>();
+			for (Course course : courses) {
+				courseList.add(course.getName());
+			}
+			dataPromo.put(promotion.toString(), courseList);
+		}
+		
+		List<Course> allCourses = courseDAO.readAllCourse();
+		for (Course course : allCourses) {
+			int id = course.getId();
+			List<Project> projects = projectDAO.readProjectByCourseId(id);
+			List<String> projectList = new ArrayList<String>();
+			for (Project project : projects) {
+				projectList.add(project.getId()+"-"+project.getSubject());
+			}
+			dataProject.put(course.getName(), projectList);
+		}
+	}
+	
+
+	public void createEmptyTeam() {
+		FacesMessage msg;
+		if (promo != null && year != null && course != null && project != null) {
+
+			Project projectObject = null;
+			int id = projectDAO.getIdFromProjectString(project);
+			List<Project> projects = projectDAO.readProjectByCourseId(id);
+			for (Project tempProject : projects) {
+				if (tempProject.toString().equals(project)) {
+					projectObject = tempProject;
+				}
+			}
+			
+			teamDAO.createEmptyTeam(projectObject, nbTeams, min, max);
+			
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Team created !", null);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid : missing information !", null);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		}
+	}
+	
+	public void onChange() {
+		if (year != null && !year.equals("")) {
+			promotions = data.get(year);
+		} else {
+			promotions = new ArrayList<String>();
+		}
+	}
+
+	public void onChangeCourse() {
+		if (promo != null && !promo.equals("")) {
+			courses = dataPromo.get(promo);
+		} else {
+			courses = new ArrayList<String>();
+		}
+	}
+
+	public void onChangeProject() {
+		if (course != null && !course.equals("")) {
+			projects = dataProject.get(course);
+		} else {
+			projects = new ArrayList<String>();
+		}
+	}
+
+	public Map<String, List<String>> getDataPromo() {
+		return dataPromo;
+	}
+
+	public void setDataPromo(Map<String, List<String>> dataPromo) {
+		this.dataPromo = dataPromo;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getPromo() {
+		return promo;
+	}
+
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+
+	public List<String> getYears() {
+		return years;
+	}
+
+	public void setYears(List<String> years) {
+		this.years = years;
+	}
+
+	public List<String> getPromotions() {
+		return promotions;
+	}
+
+	public void setPromotions(List<String> promotions) {
+		this.promotions = promotions;
+	}
+
+	public List<String> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<String> courses) {
+		this.courses = courses;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public List<Evaluation> getEvaluations() {
+		return evaluations;
+	}
+
+	public void setEvaluations(List<Evaluation> evaluations) {
+		this.evaluations = evaluations;
+	}
+
+	public ProjectDAO getProjectDAO() {
+		return projectDAO;
+	}
+
+	public void setProjectDAO(ProjectDAO projectDAO) {
+		this.projectDAO = projectDAO;
+	}
+
+	public CourseDAO getCourseDAO() {
+		return courseDAO;
+	}
+
+	public void setCourseDAO(CourseDAO courseDAO) {
+		this.courseDAO = courseDAO;
+	}
+
+	public PromotionDAO getPromoDAO() {
+		return promoDAO;
+	}
+
+	public void setPromoDAO(PromotionDAO promoDAO) {
+		this.promoDAO = promoDAO;
+	}
+
+	public UniversityYearDAO getYearDAO() {
+		return yearDAO;
+	}
+
+	public void setYearDAO(UniversityYearDAO yearDAO) {
+		this.yearDAO = yearDAO;
+	}
+
+	public Map<String, List<String>> getDataProject() {
+		return dataProject;
+	}
+
+	public void setDataProject(Map<String, List<String>> dataProject) {
+		this.dataProject = dataProject;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public List<String> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<String> projects) {
+		this.projects = projects;
+	}
+
+	public Map<String, List<String>> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, List<String>> data) {
+		this.data = data;
+	}
+
+	public TeamDAO getTeamDAO() {
+		return teamDAO;
+	}
+
+	public void setTeamDAO(TeamDAO teamDAO) {
+		this.teamDAO = teamDAO;
+	}
+
+	public int getNbTeams() {
+		return nbTeams;
+	}
+
+	public void setNbTeams(int nbTeams) {
+		this.nbTeams = nbTeams;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+	
+	
+
+	
+}
---
+++ lapis/src/business/TeamDAO.java
@@ -0,0 +1,42 @@
+package business;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import persistence.Course;
+import persistence.Evaluation;
+import persistence.EvaluationScore;
+import persistence.Project;
+import persistence.Student;
+import persistence.StudentScore;
+import persistence.Team;
+
+public class TeamDAO {
+	public void createTeam(Project project, List<Student> students, List<EvaluationScore> scores, double finalScore,
+			List<StudentScore> studentScores) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Team team = new Team(project, students, scores, finalScore, studentScores);
+		session.persist(team);
+		readTransaction.commit();
+
+	}
+	
+	public void createEmptyTeam(Project project, int amount, int min, int max) {
+		if(amount >= min && amount <= max) {
+			Session session = DBConnection.getSession();
+			Transaction readTransaction = session.beginTransaction();
+			for(int i=0; i<max; i++) {
+				Team team = new Team(project);
+				session.persist(team);
+			}
+			readTransaction.commit();
+		}
+		
+
+	}
+	
+	
+}
