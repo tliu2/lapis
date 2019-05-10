206eb7761656d6a663a697c3b5d85c59cb042aee
Antoine Leguay <antoinelg@hotmail.fr>
2019-05-10 12:06:32 +0200

    Story 6 Mid Part 3

--- lapis/src/beans/AddRowView.java
+++ lapis/src/beans/AddRowView.java
@@ -12,54 +12,93 @@ import javax.faces.context.FacesContext;
 
 import org.primefaces.event.RowEditEvent;
 
+import business.LinkCriteriaToProjectDAO;
 import persistence.Criterion;
 
-@ManagedBean(name="dtAddRowView")
+@ManagedBean(name = "dtAddRowView")
 @ViewScoped
 public class AddRowView implements Serializable {
-     
-    private List<Criterion> cars1;
- 
-    @ManagedProperty("#{carService}")
-    private CarService service;
-     
-    @PostConstruct
-    public void init() {
-        cars1 = service.createCars(15);
-    }
- 
-    public List<Criterion> getCars1() {
-        return cars1;
-    }
- 
-    public List<String> getBrands() {
-        return service.getBrands();
-    }
-     
-    public List<String> getColors() {
-        return service.getColors();
-    }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
-     
-    public void onRowEdit(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Car Edited");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
-    public void onRowCancel(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Edit Cancelled");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
- 
-    public void onAddNew() {
-        // Add one new car to the table:
-        Criterion car2Add = service.createCars(1).get(0);
-        cars1.add(car2Add);
-        FacesMessage msg = new FacesMessage("New Car added");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
+
+	private List<Criterion> cars1;
+	private List<String> carsName;
+	private List<Integer> percentages;
+	private int firstPercentage;
+
+	@ManagedProperty("#{criteriaService}")
+	private LinkCriteriaToProjectDAO service;
+
+	@PostConstruct
+	public void init() {
+		cars1 = service.readAllCriterion();
+		carsName = service.readAllCriterionName(cars1);
+		percentages = service.makePercentageList();
+		firstPercentage = percentages.get(0);
+	}
+
+	public List<Criterion> getCars1() {
+		return cars1;
+	}
+
+	public List<Integer> getPercentages() {
+		return percentages;
+	}
+
+	public void setPercentages(List<Integer> percentages) {
+		this.percentages = percentages;
+	}
+
+	public LinkCriteriaToProjectDAO getService() {
+		return service;
+	}
+
+	public void setCars1(List<Criterion> cars1) {
+		this.cars1 = cars1;
+	}
+
+	public void setService(LinkCriteriaToProjectDAO service) {
+		this.service = service;
+	}
+	
+	public int getFirstPercentage() {
+		return firstPercentage;
+	}
+
+	public void setFirstPercentage(int firstPercentage) {
+		this.firstPercentage = firstPercentage;
+	}
+
+	public List<String> getCarsName() {
+		return carsName;
+	}
+
+	public void setCarsName(List<String> carsName) {
+		this.carsName = carsName;
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Car Edited");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Edit Cancelled");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onAddNew() {
+		// Add one new car to the table
+		Criterion car2Add = service.readAllCriterion().get(0);
+		cars1.add(car2Add);
+		FacesMessage msg = new FacesMessage("New Criterion added");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
+	public void onRemoveNew() {
+		// Remove one new car to the table
+		int last = cars1.size();
+		cars1.remove(last-1);
+		FacesMessage msg = new FacesMessage("Last Criterion removed");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
 }
--- lapis/src/beans/EditView.java
+++ lapis/src/beans/EditView.java
@@ -13,61 +13,67 @@ import javax.faces.context.FacesContext;
 import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
 
+import business.LinkCriteriaToProjectDAO;
 import persistence.Criterion;
 
-@ManagedBean(name="dtEditView")
+@ManagedBean(name = "dtEditView")
 @ViewScoped
 public class EditView implements Serializable {
-     
-    private List<Criterion> cars1;
-    private List<Criterion> cars2;
-         
-    @ManagedProperty("#{carService}")
-    private CarService service;
-     
-    @PostConstruct
-    public void init() {
-        cars1 = service.createCars(10);
-        cars2 = service.createCars(10);
-    }
- 
-    public List<Criterion> getCars1() {
-        return cars1;
-    }
- 
-    public List<Criterion> getCars2() {
-        return cars2;
-    }
-     
-    public List<String> getBrands() {
-        return service.getBrands();
-    }
-     
-    public List<String> getColors() {
-        return service.getColors();
-    }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
-     
-    public void onRowEdit(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Car Edited");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
-    public void onRowCancel(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Edit Cancelled");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
-    public void onCellEdit(CellEditEvent event) {
-        Object oldValue = event.getOldValue();
-        Object newValue = event.getNewValue();
-         
-        if(newValue != null && !newValue.equals(oldValue)) {
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
-            FacesContext.getCurrentInstance().addMessage(null, msg);
-        }
-    }
+
+	private List<Criterion> cars1;
+	private List<Criterion> cars2;
+
+	@ManagedProperty("#{criteriaService}")
+	private LinkCriteriaToProjectDAO service;
+
+	@PostConstruct
+	public void init() {
+		cars1 = service.readAllCriterion();
+		cars2 = service.readAllCriterion();
+	}
+
+	public List<Criterion> getCars1() {
+		return cars1;
+	}
+
+	public List<Criterion> getCars2() {
+		return cars2;
+	}
+
+	public void setService(LinkCriteriaToProjectDAO service) {
+		this.service = service;
+	}
+
+	public LinkCriteriaToProjectDAO getService() {
+		return service;
+	}
+
+	public void setCars1(List<Criterion> cars1) {
+		this.cars1 = cars1;
+	}
+
+	public void setCars2(List<Criterion> cars2) {
+		this.cars2 = cars2;
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Criterion Edited");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Edit Cancelled");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onCellEdit(CellEditEvent event) {
+		Object oldValue = event.getOldValue();
+		Object newValue = event.getNewValue();
+
+		if (newValue != null && !newValue.equals(oldValue)) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed",
+					"Old: " + oldValue + ", New:" + newValue);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+	}
 }
