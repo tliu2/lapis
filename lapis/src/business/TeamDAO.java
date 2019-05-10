ae6531ffd117dfff5bb4f273d298f9523e320dd8
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-05-10 12:04:55 +0200

    Corrected teamDAO

--- lapis/src/business/TeamDAO.java
+++ lapis/src/business/TeamDAO.java
@@ -2,6 +2,7 @@ package business;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -13,10 +14,10 @@ import persistence.Team;
 
 public class TeamDAO {
 	public void createTeam(Project project, List<Student> students, List<EvaluationScore> scores, double finalScore,
-			List<StudentScore> studentScores) {
+			List<StudentScore> studentScores, String name) {
 		Session session = DBConnection.getSession();
 		Transaction readTransaction = session.beginTransaction();
-		Team team = new Team(project, students, scores, finalScore, studentScores);
+		Team team = new Team(project, students, scores, finalScore, studentScores, name);
 		session.persist(team);
 		readTransaction.commit();
 
@@ -32,9 +33,29 @@ public class TeamDAO {
 			}
 			readTransaction.commit();
 		}
-		
-
 	}
 	
+	public List<Team> readTeamByProjectId(int id) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Query readQuery = session.createQuery("from Team t where t.project.id = :id");
+		readQuery.setInteger("id", id);
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	
+	public List<Team> readTeamById(int id) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Query readQuery = session.createQuery("from Team t where t.id = :id");
+		readQuery.setInteger("id", id);
+		List result = readQuery.list();
+		readTransaction.commit();
+		
+		return result;
+	}
 	
 }
