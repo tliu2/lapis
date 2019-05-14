097f8d4eea62249c8a71d8eb2a4d7a3f4d4219b4
Antoine Leguay <antoinelg@hotmail.fr>
2019-05-14 10:14:12 +0200

    Story 6 mid part 6

--- lapis/src/beans/CriterionTabBean.java
+++ lapis/src/beans/CriterionTabBean.java
@@ -10,17 +10,19 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
 
 import business.EvaluationDAO;
 import business.LinkCriteriaToProjectDAO;
 import persistence.Criterion;
 
-@ManagedBean(name = "dtAddRowView")
+@ManagedBean(name = "critTab")
 @ViewScoped
-public class AddRowView implements Serializable {
+public class CriterionTabBean implements Serializable {
 
 	private List<Criterion> cars1;
+	private List<Criterion> cars2;
 	private List<String> carsName;
 	private List<Integer> percentages;
 	private int firstPercentage;
@@ -33,6 +35,7 @@ public class AddRowView implements Serializable {
 	@PostConstruct
 	public void init() {
 		cars1 = service.readAllCriterion();
+		cars2 = service.readAllCriterion();
 		carsName = service.readAllCriterionName(cars1);
 		percentages = service.makePercentageList();
 		firstPercentage = percentages.get(0);
@@ -42,6 +45,22 @@ public class AddRowView implements Serializable {
 		return cars1;
 	}
 
+	public List<Criterion> getCars2() {
+		return cars2;
+	}
+
+	public void setCars2(List<Criterion> cars2) {
+		this.cars2 = cars2;
+	}
+
+	public EvaluationDAO getEvaluationDAO() {
+		return evaluationDAO;
+	}
+
+	public void setEvaluationDAO(EvaluationDAO evaluationDAO) {
+		this.evaluationDAO = evaluationDAO;
+	}
+	
 	public List<Integer> getPercentages() {
 		return percentages;
 	}
@@ -87,6 +106,17 @@ public class AddRowView implements Serializable {
 		FacesMessage msg = new FacesMessage("Edit Cancelled");
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
+	
+	public void onCellEdit(CellEditEvent event) {
+		Object oldValue = event.getOldValue();
+		Object newValue = event.getNewValue();
+
+		if (newValue != null && !newValue.equals(oldValue)) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed",
+					"Old: " + oldValue + ", New:" + newValue);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+	}
 
 	public void onAddNew() {
 		// Add one new car to the table
@@ -98,9 +128,16 @@ public class AddRowView implements Serializable {
 	
 	public void createEvaluation() {
 		FacesMessage msg;
-		evaluationDAO.createEvaluation(cars1, percentages);
-		msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Evaluation Created !", null);
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-
+		if (cars1 == null) {
+			System.out.println("Cars1 est null");
+		}else {
+			if (percentages == null) {
+				System.out.println("Liste de pourcentages est null");
+			}else {
+				evaluationDAO.createEvaluation(cars1, percentages);
+				msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Evaluation Created !", null);
+				FacesContext.getCurrentInstance().addMessage(null, msg);
+			}
+		}
 	}
 }
