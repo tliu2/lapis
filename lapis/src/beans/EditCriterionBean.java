7993c2d09aaa567ace9156ca46bbbe2b3bd31314
mvoisin78 <mvoisin@hotmail.fr>
2019-05-15 12:01:19 +0200

    Story 5.1

---
+++ lapis/src/beans/EditCriterionBean.java
@@ -0,0 +1,86 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.RowEditEvent;
+
+import business.*;
+import persistence.*;
+
+
+@ManagedBean
+@ViewScoped
+public class EditCriterionBean {
+
+	private List<String> criterions = new ArrayList<String>();
+	private String selectedCriterion;
+	private String description;
+	
+	private Criterion criterion;
+
+	private CriterionDAO criterionDAO = new CriterionDAO();
+
+	
+	public EditCriterionBean() {
+	}
+
+	@PostConstruct
+	public void init() {
+		List<Criterion> criterionList = criterionDAO.readAllCriterion();
+		for(Criterion criterion: criterionList) {
+			criterions.add(criterion.getName());
+		}
+	}
+	
+	public void updateDescription() {
+		criterion = criterionDAO.getCriterionByName(selectedCriterion).get(0);
+		description = criterion.getDescription();
+	}
+
+	public void updateCriterion() {
+		//criterion.setDescription(description);
+		System.out.println(description + selectedCriterion);
+		criterionDAO.updateDescription(description, selectedCriterion);
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSelectedCriterion() {
+		return selectedCriterion;
+	}
+
+	public void setSelectedCriterion(String selectedCriterion) {
+		this.selectedCriterion = selectedCriterion;
+	}
+
+	public Criterion getCriterion() {
+		return criterion;
+	}
+
+	public void setCriterion(Criterion criterion) {
+		this.criterion = criterion;
+	}
+
+	public List<String> getCriterions() {
+		return criterions;
+	}
+
+	public void setCriterions(List<String> criterions) {
+		this.criterions = criterions;
+	}
+
+}
--- lapis/src/business/CriterionDAO.java
+++ lapis/src/business/CriterionDAO.java
@@ -1,9 +1,14 @@
 package business;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import persistence.Course;
 import persistence.Criterion;
+import persistence.ProjectInfo;
 
 public class CriterionDAO {
 	
@@ -15,4 +20,35 @@ public class CriterionDAO {
 		readTransaction.commit();
 	}
 	
+	public List<Criterion> readAllCriterion() {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+
+		Query readQuery = session.createQuery("from Criterion");
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	public List<Criterion> getCriterionByName(String name) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Query readQuery = session.createQuery("from Criterion c where c.name = :name");
+		readQuery.setString("name", name);
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	public void updateDescription(String description, String name) {
+		Session session = DBConnection.getSession();
+		Transaction updateTransaction = session.beginTransaction();
+		Query updateQuery = session.createQuery("update Criterion c set c.description = :description where c.name=:name");
+		updateQuery.setString("description", description);
+		updateQuery.setString("name", name);
+		updateQuery.executeUpdate();
+		updateTransaction.commit();
+	}
 }
