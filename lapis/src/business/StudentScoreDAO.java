6b1df67c8c14a79dff23d068fd204948e6bacf54
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-06-14 11:31:11 +0200

    fixes to dao and index

--- lapis/src/business/StudentScoreDAO.java
+++ lapis/src/business/StudentScoreDAO.java
@@ -14,15 +14,6 @@ import persistence.StudentScore;
 import persistence.Team;
 
 public class StudentScoreDAO {
-
-	public void createStudent(Promotion promotion, String firstname, String lastname, String ine, String ucpNumber) {
-		Session session = DBConnection.getSession();
-		Transaction readTransaction = session.beginTransaction();
-		Student student = new Student(firstname, lastname, ine, ucpNumber, promotion);
-		session.persist(student);
-		readTransaction.commit();
-
-	}
 	
 	public List<StudentScore> getStudentScoreListOfStudent(Student student){
 		Session session = DBConnection.getSession();
@@ -34,46 +25,6 @@ public class StudentScoreDAO {
 		readTransaction.commit();
 
 		return result;
-	}
-	
-	public List<Student> readAllStudent() {
-		Session session = DBConnection.getSession();
-		Transaction readTransaction = session.beginTransaction();
-
-		Query readQuery = session.createQuery("from Student");
-		List result = readQuery.list();
-		readTransaction.commit();
-
-		return result;
-	}
-	
-	public List<Student> readStudentByPromoId(int id) {
-		Session session = DBConnection.getSession();
-		Transaction readTransaction = session.beginTransaction();
-		Query readQuery = session.createQuery("from Student s where s.promotion.id = :id");
-		readQuery.setInteger("id", id);
-		List result = readQuery.list();
-		readTransaction.commit();
-
-		return result;
-	}
-	
-	public List<Student> readStudentById(int id) {
-		Session session = DBConnection.getSession();
-		Transaction readTransaction = session.beginTransaction();
-		Query readQuery = session.createQuery("from Student s where s.id = :id");
-		readQuery.setInteger("id", id);
-		List result = readQuery.list();
-		readTransaction.commit();
-
-		return result;
-	}
-
-	public void updateStudent(Student student, Session session) {
-		Transaction updateTransaction = session.beginTransaction();
-		session.update(student);
-		session.flush();
-		updateTransaction.commit();
 	}
 
 }
