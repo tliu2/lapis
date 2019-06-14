e8cece6c0dc6da5ae8bd46165a24919ecdee2f9a
oumss <36635145+oumss@users.noreply.github.com>
2019-06-14 13:28:56 +0200

    story 28

--- lapis/src/beans/StudentsCriterionNotesBestBean.java
+++ lapis/src/beans/StudentsCriterionNotesBestBean.java
@@ -39,15 +39,14 @@ import persistence.Team;
 public class StudentsCriterionNotesBestBean {
 
 	private BarChartModel barModel;
-	
+
 	private TeamDAO teamDAO = new TeamDAO();
 	private StudentDAO studentDAO = new StudentDAO();
-	
+
 	private Session session = DBConnection.getSession();
 
 	public StudentsCriterionNotesBestBean() {
 	}
-
 
 	@PostConstruct
 	public void init() {
@@ -55,83 +54,82 @@ public class StudentsCriterionNotesBestBean {
 	}
 
 	public BarChartModel getBarModel() {
-        return barModel;
-    }
-	
+		return barModel;
+	}
+
 	private void createBarModel() {
-        barModel = initBarModel();
- 
-        barModel.setTitle("Bar Chart");
-        barModel.setLegendPosition("ne");
- 
-        Axis xAxis = barModel.getAxis(AxisType.X);
-        xAxis.setLabel("Criterion");
- 
-        Axis yAxis = barModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Note");
-        yAxis.setMin(0);
-        yAxis.setMax(20);
-    }
-	
+		barModel = initBarModel();
+
+		barModel.setTitle("Bar Chart");
+		barModel.setLegendPosition("ne");
+
+		Axis xAxis = barModel.getAxis(AxisType.X);
+		xAxis.setLabel("Criterion");
+
+		Axis yAxis = barModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Note");
+		yAxis.setMin(0);
+		yAxis.setMax(20);
+	}
+
 	private BarChartModel initBarModel() {
-	
-		HashMap<String,Integer> criterionMap = new HashMap<String,Integer>();
-		HashMap<String,Integer> occurenceMap = new HashMap<String,Integer>();
+
+		HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
+		HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
 		BarChartModel model = new BarChartModel();
 		ChartSeries criterionBar = new ChartSeries();
-		List<Team> allTeamList = teamDAO.readAllTeam(session); 
-		List<Team> teamList =  new ArrayList<Team>();
-		Student student = (Student) (studentDAO.readStudentById(7,session).get(0));
-		
-		for(Team currentTeam : allTeamList) {
-			if(currentTeam.getStudents().contains(student)) {
+		List<Team> allTeamList = teamDAO.readAllTeam(session);
+		List<Team> teamList = new ArrayList<Team>();
+		Student student = (Student) (studentDAO.readStudentById(7, session).get(0));
+
+		for (Team currentTeam : allTeamList) {
+			if (currentTeam.getStudents().contains(student)) {
 				teamList.add(currentTeam);
 			}
 		}
-		
-		for(Team team : teamList) {
-			
+
+		for (Team team : teamList) {
+
 			int index = 0;
-			for(Student currentStudent : team.getStudents()) {
-				
+			for (Student currentStudent : team.getStudents()) {
+
 				index++;
-				if(currentStudent.getIne() == student.getIne()) {
-					
+				if (currentStudent.getIne() == student.getIne()) {
+
 					List<StudentScore> studentScoreList = team.getStudentScores();
-					StudentScore studentScore = studentScoreList.get(index-1); 
+					StudentScore studentScore = studentScoreList.get(index - 1);
 					List<EvaluationScore> evaluationScorelist = studentScore.getScores();
-					criterionBar.setLabel(student.getFirstname() +' ' + student.getLastname());
-					for(EvaluationScore evaluationScore : evaluationScorelist) {
-						
-			    	   Evaluation evaluation = evaluationScore.getEvaluation();
-			    	   String criterionName =  evaluation.getCriterion().getName();
-			    	   int score = evaluationScore.getScore();
-				    	   if(criterionMap.containsKey(criterionName)) {
-				    		   
-				    		   if(criterionMap.get(criterionName) > 10) {
-				    			   
-					    		   occurenceMap.put(criterionName, occurenceMap.get(criterionName) +1);
-					    		   criterionMap.put(criterionName, (criterionMap.get(criterionName) + score));
-				    		   }
-				    	   }
-				    	   else {
-				    		   if(score > 10) {
-				    			   
-					    		   occurenceMap.put(criterionName, 1);
-					    		   criterionMap.put(criterionName, score /(occurenceMap.get(criterionName)));
-				    		   }
-				    	   }
-			        }
+					criterionBar.setLabel(student.getFirstname() + ' ' + student.getLastname());
+					for (EvaluationScore evaluationScore : evaluationScorelist) {
+
+						Evaluation evaluation = evaluationScore.getEvaluation();
+						String criterionName = evaluation.getCriterion().getName();
+						int score = evaluationScore.getScore();
+						if (criterionMap.containsKey(criterionName)) {
+
+							if (criterionMap.get(criterionName) > 10) {
+
+								occurenceMap.put(criterionName, occurenceMap.get(criterionName) + 1);
+								criterionMap.put(criterionName, (criterionMap.get(criterionName) + score));
+							}
+						} else {
+							if (score > 10) {
+
+								occurenceMap.put(criterionName, 1);
+								criterionMap.put(criterionName, score / (occurenceMap.get(criterionName)));
+							}
+						}
+					}
 				}
 			}
 		}
-		
-		for(String criterion : criterionMap.keySet())
+
+		for (String criterion : criterionMap.keySet())
 			criterionBar.set(criterion, criterionMap.get(criterion));
-        
+
 		model.addSeries(criterionBar);
 		return model;
-    }
+	}
 
 	public TeamDAO getTeamDAO() {
 		return teamDAO;
@@ -144,5 +142,5 @@ public class StudentsCriterionNotesBestBean {
 	public void setBarModel(BarChartModel barModel) {
 		this.barModel = barModel;
 	}
-		
+
 }
