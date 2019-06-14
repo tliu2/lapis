9a5785fa0af40add989cb7f82c18953bc2faea4e
oumss <36635145+oumss@users.noreply.github.com>
2019-06-14 15:12:15 +0200

    story 28 commit for ludovico

--- lapis/src/beans/StudentsCriterionNotesBestBean.java
+++ lapis/src/beans/StudentsCriterionNotesBestBean.java
@@ -3,6 +3,7 @@ package beans;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -19,6 +20,7 @@ import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.PieChartModel;
 
 import business.CourseDAO;
 import business.DBConnection;
@@ -39,10 +41,14 @@ import persistence.Team;
 public class StudentsCriterionNotesBestBean {
 
 	private BarChartModel barModel;
-
+	
+	private Map<String, List<String>> data = new HashMap<String, List<String>>();
+	private List<String> students;
+	private String student;
+	
 	private TeamDAO teamDAO = new TeamDAO();
 	private StudentDAO studentDAO = new StudentDAO();
-
+	ChartSeries criterionBar = new ChartSeries();
 	private Session session = DBConnection.getSession();
 
 	public StudentsCriterionNotesBestBean() {
@@ -50,16 +56,17 @@ public class StudentsCriterionNotesBestBean {
 
 	@PostConstruct
 	public void init() {
+		List<Student> allStudents = studentDAO.readAllStudent();
+		students = new ArrayList<String>();
+		for (Student student : allStudents) {
+			students.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+		}
 		createBarModel();
 	}
 
-	public BarChartModel getBarModel() {
-		return barModel;
-	}
-
 	private void createBarModel() {
-		barModel = initBarModel();
-
+		barModel = new BarChartModel();
+		
 		barModel.setTitle("Bar Chart");
 		barModel.setLegendPosition("ne");
 
@@ -70,17 +77,20 @@ public class StudentsCriterionNotesBestBean {
 		yAxis.setLabel("Note");
 		yAxis.setMin(0);
 		yAxis.setMax(20);
+		
+		
 	}
 
-	private BarChartModel initBarModel() {
 
+
+	private BarChartModel initBarModel(Student student) {
+		//System.out.println(student.getFirstname()+student.getLastname()+student.getId());
 		HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
 		HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
-		BarChartModel model = new BarChartModel();
-		ChartSeries criterionBar = new ChartSeries();
+		barModel = new BarChartModel();
+		
 		List<Team> allTeamList = teamDAO.readAllTeam(session);
 		List<Team> teamList = new ArrayList<Team>();
-		Student student = (Student) (studentDAO.readStudentById(7, session).get(0));
 
 		for (Team currentTeam : allTeamList) {
 			if (currentTeam.getStudents().contains(student)) {
@@ -127,8 +137,73 @@ public class StudentsCriterionNotesBestBean {
 		for (String criterion : criterionMap.keySet())
 			criterionBar.set(criterion, criterionMap.get(criterion));
 
-		model.addSeries(criterionBar);
-		return model;
+		barModel.addSeries(criterionBar);
+		return barModel;
+	}
+
+	public void onChange() {
+		if (student != null && !student.equals("")) {
+			String[] parts = student.split(" ");
+			String studentID = parts[2];
+			
+			List <Student> studentsList = studentDAO.readStudentById(Integer.parseInt(studentID));
+			
+			initBarModel(studentsList.get(0));
+		} else {
+			//promotions = new ArrayList<String>();
+		}
+	}
+	
+	public Map<String, List<String>> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, List<String>> data) {
+		this.data = data;
+	}
+
+	public List<String> getStudents() {
+		return students;
+	}
+
+
+	public BarChartModel getBarModel() {
+		return barModel;
+	}
+
+	public void setStudents(List<String> students) {
+		this.students = students;
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+	
+	public ChartSeries getCriterionBar() {
+		return criterionBar;
+	}
+
+	public void setCriterionBar(ChartSeries criterionBar) {
+		this.criterionBar = criterionBar;
+	}
+	public StudentDAO getStudentDAO() {
+		return studentDAO;
+	}
+
+	public void setStudentDAO(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
 	}
 
 	public TeamDAO getTeamDAO() {
