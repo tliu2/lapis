597d846f593f52cb1dcffef898980be5fd7a455d
Ludovic Domingues <ludovic.domingues96@gmail.com>
2019-06-14 10:53:27 +0200

    StudentScoreDAO created and story 30

--- lapis/src/beans/StudentsDistributionNotesBean.java
+++ lapis/src/beans/StudentsDistributionNotesBean.java
@@ -1,7 +1,9 @@
 package beans;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -18,15 +20,31 @@ import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.PieChartModel;
 
 import business.CourseDAO;
+import business.CriteriaToProjectDAO;
 import business.PromotionDAO;
+import business.StudentDAO;
+import business.StudentScoreDAO;
 import business.UniversityYearDAO;
+import persistence.Course;
+import persistence.Project;
 import persistence.Promotion;
+import persistence.Student;
+import persistence.StudentScore;
+import persistence.Team;
+import persistence.UniversityYear;
 
 @ManagedBean
 public class StudentsDistributionNotesBean {
 	
 	private PieChartModel pieModel;
+	
+	private Map<String, List<String>> data = new HashMap<String, List<String>>();
+	private List<String> students;
 
+	private String student;
+	
+	private StudentDAO studentDAO = new StudentDAO();
+	private StudentScoreDAO studentScoreDAO = new StudentScoreDAO();
 
 	public StudentsDistributionNotesBean() {
 	}
@@ -34,24 +52,112 @@ public class StudentsDistributionNotesBean {
 
 	@PostConstruct
 	public void init() {
+		List<Student> allStudents = studentDAO.readAllStudent();
+		students = new ArrayList<String>();
+		for (Student student : allStudents) {
+			students.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+		}
+		
 		createPieModel();
 	}
+	
+	public void onChange() {
+		if (student != null && !student.equals("")) {
+			String[] parts = student.split(" ");
+			String studentID = parts[2];
+			
+			List <Student> studentsList = studentDAO.readStudentById(Integer.parseInt(studentID));
+			
+			createPieWithStudent(studentsList.get(0));
+		} else {
+			//promotions = new ArrayList<String>();
+		}
+	}
+	
 
     private void createPieModel() {
         pieModel = new PieChartModel();
  
-        pieModel.set("Brand 1", 540);
-        pieModel.set("Brand 2", 325);
-        pieModel.set("Brand 3", 702);
-        pieModel.set("Brand 4", 421);
+        pieModel.set("Default", 0);
  
-        pieModel.setTitle("Simple Pie");
         pieModel.setLegendPosition("w");
         pieModel.setShadow(false);
+    }
+    
+    private void createPieWithStudent(Student student) {
+    	 pieModel = new PieChartModel();
+    	 int first=0;
+    	 int second=0;
+    	 int third=0;
+    	 int fourth=0;
+    	 
+    	 List<StudentScore> studentScoreList = studentScoreDAO.getStudentScoreListOfStudent(student);
+    	 
+    	 for(StudentScore studentScore : studentScoreList) {
+    		if(studentScore.getFinalScore()<=5)
+    			first ++;
+    		else if(studentScore.getFinalScore()<=10)
+    			second++;
+    		else if(studentScore.getFinalScore()<=15)
+    			third++;
+    		else fourth++;
+    	 }
+         pieModel.set("<=5", first);
+         pieModel.set("<=10", second);
+         pieModel.set("<=15", third);
+         pieModel.set("<=20", fourth);
+  
+         pieModel.setLegendPosition("w");
+         pieModel.setShadow(false);
+    	
     }
 	
     public PieChartModel getPieModel() {
         return pieModel;
     }
+    
+    public Map<String, List<String>> getData() {
+		return data;
+	}
+
+
+	public void setData(Map<String, List<String>> data) {
+		this.data = data;
+	}
+
+
+	public List<String> getStudents() {
+		return students;
+	}
+
+
+	public void setStudents(List<String> students) {
+		this.students = students;
+	}
+
+
+	public String getStudent() {
+		return student;
+	}
+
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+
+
+	public StudentDAO getStudentDAO() {
+		return studentDAO;
+	}
+
+
+	public void setStudentDAO(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
+
+
+	public void setPieModel(PieChartModel pieModel) {
+		this.pieModel = pieModel;
+	}
 
 }
---
+++ lapis/src/business/StudentScoreDAO.java
@@ -0,0 +1,79 @@
+package business;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import persistence.Course;
+import persistence.ProjectInfo;
+import persistence.Promotion;
+import persistence.Student;
+import persistence.StudentScore;
+import persistence.Team;
+
+public class StudentScoreDAO {
+
+	public void createStudent(Promotion promotion, String firstname, String lastname, String ine, String ucpNumber) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Student student = new Student(firstname, lastname, ine, ucpNumber, promotion);
+		session.persist(student);
+		readTransaction.commit();
+
+	}
+	
+	public List<StudentScore> getStudentScoreListOfStudent(Student student){
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+
+		Query readQuery = session.createQuery("from StudentScore s where s.student.id = :id");
+		readQuery.setInteger("id", student.getId());
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	public List<Student> readAllStudent() {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+
+		Query readQuery = session.createQuery("from Student");
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	public List<Student> readStudentByPromoId(int id) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Query readQuery = session.createQuery("from Student s where s.promotion.id = :id");
+		readQuery.setInteger("id", id);
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+	
+	public List<Student> readStudentById(int id) {
+		Session session = DBConnection.getSession();
+		Transaction readTransaction = session.beginTransaction();
+		Query readQuery = session.createQuery("from Student s where s.id = :id");
+		readQuery.setInteger("id", id);
+		List result = readQuery.list();
+		readTransaction.commit();
+
+		return result;
+	}
+
+	public void updateStudent(Student student, Session session) {
+		Transaction updateTransaction = session.beginTransaction();
+		session.update(student);
+		session.flush();
+		updateTransaction.commit();
+	}
+
+}
