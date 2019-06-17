9f222ec78fda012c23ebcb40946f09bb33a60d0a
mvoisin78 <mvoisin@hotmail.fr>
2019-06-17 12:59:28 +0200

    Story 30

--- lapis/src/beans/StudentsDistributionNotesBean.java
+++ lapis/src/beans/StudentsDistributionNotesBean.java
@@ -9,11 +9,16 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import org.primefaces.model.chart.PieChartModel;
 
+import business.PromotionDAO;
 import business.StudentDAO;
 import business.StudentScoreDAO;
-
+import business.UniversityYearDAO;
+import persistence.Course;
+import persistence.Project;
+import persistence.Promotion;
 import persistence.Student;
 import persistence.StudentScore;
+import persistence.UniversityYear;
 
 @ManagedBean
 public class StudentsDistributionNotesBean {
@@ -21,9 +26,20 @@ public class StudentsDistributionNotesBean {
 	private PieChartModel pieModel;
 	
 	private Map<String, List<String>> data = new HashMap<String, List<String>>();
-	private List<String> students;
+	private Map<String, List<String>> dataStudent = new HashMap<String, List<String>>();
+	
+	private List<String> students = new ArrayList<String>();
 
 	private String student;
+	
+	private String year;
+	private String promo;
+
+	private List<String> years = new ArrayList<String>();
+	private List<String> promotions = new ArrayList<String>();
+	
+	private UniversityYearDAO yearDAO = new UniversityYearDAO();
+	private PromotionDAO promoDAO = new PromotionDAO();
 	
 	private StudentDAO studentDAO = new StudentDAO();
 	private StudentScoreDAO studentScoreDAO = new StudentScoreDAO();
@@ -34,12 +50,39 @@ public class StudentsDistributionNotesBean {
 
 	@PostConstruct
 	public void init() {
-		List<Student> allStudents = studentDAO.readAllStudent();
+		/*List<Student> allStudents = studentDAO.readAllStudent();
 		students = new ArrayList<String>();
 		for (Student student : allStudents) {
 			students.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
 		}
+		*/
+		List<UniversityYear> allYears = yearDAO.readAllUniversityYears();
+		years = new ArrayList<String>();
+		for (UniversityYear universityYear : allYears) {
+			years.add(universityYear.toString());
+		}
+
+		for (UniversityYear universityYear : allYears) {
+			int id = universityYear.getId();
+			List<Promotion> promos = promoDAO.readPromoByYearId(id);
+			List<String> promoList = new ArrayList<String>();
+			for (Promotion promo : promos) {
+				promoList.add(promo.toString());
+			}
+			data.put(universityYear.toString(), promoList);
+		}
 		
+		List<Promotion> allPromos = promoDAO.readAllPromo();
+		for (Promotion promotion : allPromos) {
+			int id = promotion.getId();
+			List<Student> students = studentDAO.readStudentByPromoId(id);
+			List<String> studentList = new ArrayList<String>();
+			for (Student student : students) {
+				studentList.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+				System.out.println(student.getFirstname());
+			}
+			dataStudent.put(promotion.toString(), studentList);
+		}
 		createPieModel();
 	}
 	
@@ -56,6 +99,24 @@ public class StudentsDistributionNotesBean {
 		}
 	}
 	
+	public void onChangeUY() {
+		if (year != null && !year.equals("")) {
+			promotions = data.get(year);
+			System.out.println(year);
+		} else {
+			promotions = new ArrayList<String>();
+		}
+	}
+	
+	public void onChangePromo() {
+		if (promo != null && !promo.equals("")) {
+			students = dataStudent.get(promo.toString());
+			System.out.println(promo.toString() +"   -    "+promo);
+		} else {
+			students = new ArrayList<String>();
+			System.out.println("raté");
+		}
+	}
 
     private void createPieModel() {
         pieModel = new PieChartModel();
@@ -140,6 +201,46 @@ public class StudentsDistributionNotesBean {
 
 	public void setPieModel(PieChartModel pieModel) {
 		this.pieModel = pieModel;
+	}
+
+
+	public String getYear() {
+		return year;
+	}
+
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+
+	public String getPromo() {
+		return promo;
+	}
+
+
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+
+
+	public List<String> getYears() {
+		return years;
+	}
+
+
+	public void setYears(List<String> years) {
+		this.years = years;
+	}
+
+
+	public List<String> getPromotions() {
+		return promotions;
+	}
+
+
+	public void setPromotions(List<String> promotions) {
+		this.promotions = promotions;
 	}
 
 }
