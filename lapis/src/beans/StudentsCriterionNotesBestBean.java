c8d910a922923d38df6d689eff7ecceac21f98b2
oumss <36635145+oumss@users.noreply.github.com>
2019-06-20 16:07:17 +0200

    stories 28 29 & 30

--- lapis/src/beans/StudentsCriterionNotesBestBean.java
+++ lapis/src/beans/StudentsCriterionNotesBestBean.java
@@ -7,46 +7,87 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.hibernate.Session;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.PieChartModel;
 
 import business.DBConnection;
+import business.PromotionDAO;
 import business.StudentDAO;
+import business.StudentScoreDAO;
 import business.TeamDAO;
+import business.UniversityYearDAO;
 import persistence.Evaluation;
 import persistence.EvaluationScore;
+import persistence.Promotion;
 import persistence.Student;
 import persistence.StudentScore;
 import persistence.Team;
+import persistence.UniversityYear;
 
 @ManagedBean
+@ViewScoped
 public class StudentsCriterionNotesBestBean {
 
-	private BarChartModel barModel = new BarChartModel();;
-	HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
-	HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
+	private BarChartModel barModel = new BarChartModel();
+	
+	private HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
+	private HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
 	private Map<String, List<String>> data = new HashMap<String, List<String>>();
-	private List<String> students;
+	private Map<String, List<String>> dataStudent = new HashMap<String, List<String>>();
+	
+	
+	private ChartSeries criterionBar = new ChartSeries();
+	private Session session = DBConnection.getSession();
+	
 	private String student;
+	private String year;
+	private String promo;
+
+	private List<String> years = new ArrayList<String>();
+	private List<String> promotions = new ArrayList<String>();
+	private List<String> students = new ArrayList<String>();
 	
 	private TeamDAO teamDAO = new TeamDAO();
 	private StudentDAO studentDAO = new StudentDAO();
-	ChartSeries criterionBar = new ChartSeries();
-	private Session session = DBConnection.getSession();
+	private UniversityYearDAO yearDAO = new UniversityYearDAO();
+	private PromotionDAO promoDAO = new PromotionDAO();
 
 	public StudentsCriterionNotesBestBean() {
 	}
 
 	@PostConstruct
 	public void init() {
-		List<Student> allStudents = studentDAO.readAllStudent();
-		students = new ArrayList<String>();
-		for (Student student : allStudents) {
-			students.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+		List<UniversityYear> allYears = yearDAO.readAllUniversityYears();
+		years = new ArrayList<String>();
+		for (UniversityYear universityYear : allYears) {
+			years.add(universityYear.toString());
+		}
+
+		for (UniversityYear universityYear : allYears) {
+			int id = universityYear.getId();
+			List<Promotion> promos = promoDAO.readPromoByYearId(id);
+			List<String> promoList = new ArrayList<String>();
+			for (Promotion promo : promos) {
+				promoList.add(promo.toString());
+			}
+			data.put(universityYear.toString(), promoList);
+		}
+		
+		List<Promotion> allPromos = promoDAO.readAllPromo();
+		for (Promotion promotion : allPromos) {
+			int id = promotion.getId();
+			List<Student> students = studentDAO.readStudentByPromoId(id);
+			List<String> studentList = new ArrayList<String>();
+			for (Student student : students) {
+				studentList.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+			}
+			dataStudent.put(promotion.toString(), studentList);
 		}
 		createBarModel();
 	}
@@ -105,7 +146,7 @@ public class StudentsCriterionNotesBestBean {
 						}else {
 							if (score >= 10) {
 								occurenceMap.put(criterionName, 1);
-								criterionMap.put(criterionName, score / (occurenceMap.get(criterionName)));
+								criterionMap.put(criterionName, (score / (occurenceMap.get(criterionName))));
 							}
 						}
 					}
@@ -125,6 +166,22 @@ public class StudentsCriterionNotesBestBean {
 		barModel = new BarChartModel();
 		barModel.addSeries(criterionBar);
 		return barModel;
+	}
+	
+	public void onChangeUY() {
+		if (year != null && !year.equals("")) {
+			promotions = data.get(year);
+		} else {
+			promotions = new ArrayList<String>();
+		}
+	}
+	
+	public void onChangePromo() {
+		if (promo != null && !promo.equals("")) {
+			students = dataStudent.get(promo);
+		}else {
+			students = new ArrayList<String>();
+		}
 	}
 	
 	public void onChange() {
@@ -147,6 +204,62 @@ public class StudentsCriterionNotesBestBean {
 		return false;
 	}
 	
+	public Map<String, List<String>> getDataStudent() {
+		return dataStudent;
+	}
+
+	public void setDataStudent(Map<String, List<String>> dataStudent) {
+		this.dataStudent = dataStudent;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getPromo() {
+		return promo;
+	}
+
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+
+	public List<String> getYears() {
+		return years;
+	}
+
+	public void setYears(List<String> years) {
+		this.years = years;
+	}
+
+	public List<String> getPromotions() {
+		return promotions;
+	}
+
+	public void setPromotions(List<String> promotions) {
+		this.promotions = promotions;
+	}
+
+	public UniversityYearDAO getYearDAO() {
+		return yearDAO;
+	}
+
+	public void setYearDAO(UniversityYearDAO yearDAO) {
+		this.yearDAO = yearDAO;
+	}
+
+	public PromotionDAO getPromoDAO() {
+		return promoDAO;
+	}
+
+	public void setPromoDAO(PromotionDAO promoDAO) {
+		this.promoDAO = promoDAO;
+	}
+
 	public HashMap<String, Integer> getCriterionMap() {
 		return criterionMap;
 	}
--- lapis/src/beans/StudentsCriterionNotesWorstBean.java
+++ lapis/src/beans/StudentsCriterionNotesWorstBean.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.hibernate.Session;
 import org.primefaces.model.chart.Axis;
@@ -15,38 +16,77 @@ import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 
 import business.DBConnection;
+import business.PromotionDAO;
 import business.StudentDAO;
 import business.TeamDAO;
+import business.UniversityYearDAO;
 import persistence.Evaluation;
 import persistence.EvaluationScore;
+import persistence.Promotion;
 import persistence.Student;
 import persistence.StudentScore;
 import persistence.Team;
+import persistence.UniversityYear;
 
 @ManagedBean
+@ViewScoped
 public class StudentsCriterionNotesWorstBean {
 	
 	private BarChartModel barModel = new BarChartModel();;
-	HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
-	HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
+
+	private HashMap<String, Integer> criterionMap = new HashMap<String, Integer>();
+	private HashMap<String, Integer> occurenceMap = new HashMap<String, Integer>();
 	private Map<String, List<String>> data = new HashMap<String, List<String>>();
-	private List<String> students;
+	private Map<String, List<String>> dataStudent = new HashMap<String, List<String>>();
+	
+	
+	private ChartSeries criterionBar = new ChartSeries();
+	private Session session = DBConnection.getSession();
+	
 	private String student;
+	private String year;
+	private String promo;
+
+	private List<String> years = new ArrayList<String>();
+	private List<String> promotions = new ArrayList<String>();
+	private List<String> students = new ArrayList<String>();
 	
 	private TeamDAO teamDAO = new TeamDAO();
 	private StudentDAO studentDAO = new StudentDAO();
-	ChartSeries criterionBar = new ChartSeries();
-	private Session session = DBConnection.getSession();
-
+	private UniversityYearDAO yearDAO = new UniversityYearDAO();
+	private PromotionDAO promoDAO = new PromotionDAO();
+	
+	
 	public StudentsCriterionNotesWorstBean() {
 	}
 
 	@PostConstruct
 	public void init() {
-		List<Student> allStudents = studentDAO.readAllStudent();
-		students = new ArrayList<String>();
-		for (Student student : allStudents) {
-			students.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+		List<UniversityYear> allYears = yearDAO.readAllUniversityYears();
+		years = new ArrayList<String>();
+		for (UniversityYear universityYear : allYears) {
+			years.add(universityYear.toString());
+		}
+
+		for (UniversityYear universityYear : allYears) {
+			int id = universityYear.getId();
+			List<Promotion> promos = promoDAO.readPromoByYearId(id);
+			List<String> promoList = new ArrayList<String>();
+			for (Promotion promo : promos) {
+				promoList.add(promo.toString());
+			}
+			data.put(universityYear.toString(), promoList);
+		}
+		
+		List<Promotion> allPromos = promoDAO.readAllPromo();
+		for (Promotion promotion : allPromos) {
+			int id = promotion.getId();
+			List<Student> students = studentDAO.readStudentByPromoId(id);
+			List<String> studentList = new ArrayList<String>();
+			for (Student student : students) {
+				studentList.add(student.getFirstname()+" "+student.getLastname()+ " "+student.getId());
+			}
+			dataStudent.put(promotion.toString(), studentList);
 		}
 		createBarModel();
 	}
@@ -128,6 +168,22 @@ public class StudentsCriterionNotesWorstBean {
 		return barModel;
 	}
 	
+	public void onChangeUY() {
+		if (year != null && !year.equals("")) {
+			promotions = data.get(year);
+		} else {
+			promotions = new ArrayList<String>();
+		}
+	}
+	
+	public void onChangePromo() {
+		if (promo != null && !promo.equals("")) {
+			students = dataStudent.get(promo);
+		}else {
+			students = new ArrayList<String>();
+		}
+	}
+	
 	public void onChange() {
 		
 		if (this.student != null && !this.student.equals("")) {
@@ -148,6 +204,62 @@ public class StudentsCriterionNotesWorstBean {
 		return false;
 	}
 	
+	public Map<String, List<String>> getDataStudent() {
+		return dataStudent;
+	}
+
+	public void setDataStudent(Map<String, List<String>> dataStudent) {
+		this.dataStudent = dataStudent;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getPromo() {
+		return promo;
+	}
+
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+
+	public List<String> getYears() {
+		return years;
+	}
+
+	public void setYears(List<String> years) {
+		this.years = years;
+	}
+
+	public List<String> getPromotions() {
+		return promotions;
+	}
+
+	public void setPromotions(List<String> promotions) {
+		this.promotions = promotions;
+	}
+
+	public UniversityYearDAO getYearDAO() {
+		return yearDAO;
+	}
+
+	public void setYearDAO(UniversityYearDAO yearDAO) {
+		this.yearDAO = yearDAO;
+	}
+
+	public PromotionDAO getPromoDAO() {
+		return promoDAO;
+	}
+
+	public void setPromoDAO(PromotionDAO promoDAO) {
+		this.promoDAO = promoDAO;
+	}
+
 	public HashMap<String, Integer> getCriterionMap() {
 		return criterionMap;
 	}
